3b6075564fc682a1f43734409df93ad2fa2a9ab4
zurikotsiklauri@gmail.com <zurikotsiklauri@gmail.com>
2023-07-09 23:18:33 +0400

    Change Name to FirstName and add LastName property

--- GitExtension/GitExtension/Person.cs
+++ GitExtension/GitExtension/Person.cs
@@ -8,6 +8,7 @@ namespace GitExtension
 {
     public class Person
     {
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
